e7ec3ddd350409ee7f1c32496316cac4fdabee76
&! Tekky#1337 <98614666+xtekky@users.noreply.github.com>
2022-07-20 01:43:59 +0200

    Create api.py

---
+++ api.py
@@ -0,0 +1,9 @@
+gorgon = requests.post(
+    url = 'https://xtekky.com/xgorgon/v2',
+    data = {
+        "params": "userid=xxxx&version=9293&etc...",
+        "data": None, # POST data (urlencoded)
+        "cookies": "sessid=xxxxx; odin-tt=xxxxx;"
+    }
+).json()
+print(gorgon.json())
