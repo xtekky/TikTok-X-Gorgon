dc88c0217dc83a681a5fc1b7b290a48a05839c0b
&! Tekky#1337 <98614666+xtekky@users.noreply.github.com>
2022-07-23 22:40:32 +0200

    Update api.py

--- api.py
+++ api.py
@@ -1,11 +1,1 @@
-import requests
-
-gorgon = requests.post(
-    url = 'https://xtekky.com/xgorgon/v2',
-    data = {
-        "params": "userid=xxxx&version=9293&etc...",
-        "data": None, # POST data (urlencoded)
-        "cookies": "sessid=xxxxx; odin-tt=xxxxx;"
-    }
-).json()
-print(gorgon)
+dm t.me/xtekky for demo
